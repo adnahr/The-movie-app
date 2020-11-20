58f3478f1d08cf3e8cc8118352ca5da6b26e5e19
adna hrnjic <adnahrnjic2@gmail.com>
2020-11-20 15:42:27 +0100

    Final

--- src/Components/Movie/MovieList.tsx
+++ src/Components/Movie/MovieList.tsx
@@ -3,7 +3,7 @@ import Card from '../Card';
 import { MovieType } from '../../Types';
 
 const MovieList: React.FC<{ movies: MovieType[] }> = ({ movies }) => {
-	if (movies.length == 0) return <div>No movies avaliable! </div>;
+	if (movies.length == 0) return <div className="no-avaliable-list">No movies avaliable! </div>;
 	return (
 		<div className="containter">
 			<div className="list">
